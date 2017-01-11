e3c7619cd8e8de851bc0e1cd41a97cee4a3f3a57
Dylan Rodriquez <dylanmr@uchicago.edu>
2017-01-10 19:55:18 -0600

    Working 19

--- Chapter1/1.5/replaceblanks19.c
+++ Chapter1/1.5/replaceblanks19.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /*write a program to copy its input to
 its output, replacing each string of one
 or more blanks by a single blank*/
@@ -11,10 +12,19 @@ int main(){
         if (prevc != ' '){
           putchar(c);
         }
+/*|   |  c c c c - - - - c c
+  | c |  0 1 2 3 4 5 6 7 8 9
+  | p |  n 0 1 2 3 4 5 6 7 8
+c is set first, proceeds through loop
+then prevc set. second time, prevc has val
+so comparison available. if nonblank char
+second if statement, no comparison needed.
+if blank statement, comparison needed.
+*/
       }
-      else{
+      if (c != ' '){
         putchar(c);
       }
+      prevc = c;
     }
-    printf("%d\n", c);
 }
